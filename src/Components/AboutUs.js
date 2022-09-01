58e9d57180205ac956ece2486f4438f00bdfc84e
Tanner <troxs690@gmail.com>
2022-09-01 16:00:25 -0500

    Fixed form layout

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -35,8 +35,8 @@ function AboutUs() {
       <div className="about">
         Hello, My name is Tanner, I am the owner of Midwest Classic Trucks LLC.
         We have been in the classic truck business for 25 years! If you have any
-        questions or are looking to finance, Give my office a call at
-        612-233-4433 we will be happy to help!
+        questions or looking to finance. Give my office a call at
+        612-233-4433, we will be happy to help!
         <ul className="no-bullets">
           <li>Midwest Classic Trucks</li>
           <li>123 Main Street.</li>
--- src/Components/NewTruckForm.js
+++ src/Components/NewTruckForm.js
@@ -1,36 +1,34 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function NewTruckForm() {
-
   return (
+    <div className="input-form">
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" placeholder="Year" variant="outlined" />
+        <TextField id="outlined-basic" placeholder="Make" variant="outlined" />
+        <br></br>
 
-<Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
+        <TextField id="outlined-basic" placeholder="Model" variant="outlined" />
+        <TextField id="outlined-basic" placeholder="Image Url" variant="outlined" />
+        <br></br>
+        <TextField id="outlined-basic" placeholder="Mileage" variant="outlined" />
+        <TextField id="outlined-basic" placeholder="Price" variant="outlined" />
+        <br></br>
+          <Button  className="submit-btn" variant="contained">Submit</Button>
         
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      
-      <TextField id="outlined-basic" label="Year" variant="outlined" />
-      <TextField id="outlined-basic" label="Make" variant="outlined" />
-     
-      <TextField id="outlined-basic" label="Model" variant="outlined" />
-      <TextField id="outlined-basic" label="Image Url" variant="outlined" />
-      <TextField id="outlined-basic" label="Mileage" variant="outlined" />
-      <TextField id="outlined-basic" label="Price" variant="outlined" />
-      <Stack spacing={2} direction="row">
-      <Button variant="contained">Submit</Button>
-      
-    </Stack>
-  
-    </Box>
+      </Box>
+    </div>
   );
 }
-export default NewTruckForm
+export default NewTruckForm;
