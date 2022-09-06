3e1e37bcb8e6f01e7000aef74dcde17fc2957e67
Tanner <troxs690@gmail.com>
2022-09-06 13:42:39 -0500

    update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,9 +5,11 @@ function Home() {
     <div className="HomePage">
       <h1>
         Welcome to Midwest Classic Trucks LLC,
-        <br></br>Your one stop shop to buy/sell your classic Truck!
+        <br></br>
+        Your one stop shop to buy/sell Classic Trucks!
         <br></br>
         We are proudly the biggest Classic truck dealership in the Midwest!
+       
       </h1>
     </div>
   );
